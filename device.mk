1a13c02a3f18c89a9cb0dc2f21a4417d4b7e7cec
Miguel Martinez <slverwolf@gmail.com>
2018-05-11 11:02:12 -0500

    Zuk: Enable building opencv libs
    Change-Id: I92e4f2b6dba87558daeb690273e4a8ed6798b207

--- device.mk
+++ device.mk
@@ -106,7 +106,13 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libvolumelistener \
-    tinymix
+    tinymix \
+    libcxcore \
+    libcv \
+    libcvaux \
+    libcvhighgui \
+    libcvml \
+    libopencv
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
