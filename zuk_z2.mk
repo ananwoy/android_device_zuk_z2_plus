19a57297c7b5739c1c158a9b0d2dd5c9e424d95a
MRTHAKER <mahek.haker@gmail.com>
2018-07-11 16:57:43 +0530

    Common: Cleanup

---
+++ zuk_z2.mk
@@ -0,0 +1,9 @@
+
+
+# whitelisted app
+PRODUCT_COPY_FILES += \
+    device/qcom/common/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
+PRODUCT_COPY_FILES += \
+    device/qcom/common/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
